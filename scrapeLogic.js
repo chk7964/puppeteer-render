d7afa6a8814211e10b239184abf371c62ff0c428
Chandradeep Kumar <developer.chk7964@gmail.com>
2024-12-06 09:35:21 +0530

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,29 +17,29 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
-
+    await page.goto("https://api.ainickfinder.com/");
+  const html = await page.content();
     // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+   // await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+   // await page.type(".search-box__input", "automate beyond recorder");
 
     // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+   // const searchResultSelector = ".search-box__link";
+   // await page.waitForSelector(searchResultSelector);
+   // await page.click(searchResultSelector);
 
     // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+   // const textSelector = await page.waitForSelector(
+   //   "text/Customize and automate"
+   // );
+   // const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
     // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+   // const logStatement = `The title of this blog post is ${fullTitle}`;
+ //   console.log(logStatement);
+    res.send(html);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
